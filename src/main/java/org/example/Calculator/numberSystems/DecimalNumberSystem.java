09a53a7e926c5b55259a6e6bc8f8df83959ff852
ErKoMi <konst_ermakov03@mail.ru>
2023-04-30 00:14:48 +0300

    Добавил системы счисления

--- src/main/java/org/example/Calculator/numberSystems/DecimalNumberSystem.java
+++ src/main/java/org/example/Calculator/numberSystems/DecimalNumberSystem.java
@@ -1,2 +1,21 @@
-package org.example.Calculator.numberSystems;public class DecimalNumberSystem {
+package org.example.Calculator.numberSystems;
+
+import java.util.regex.Pattern;
+
+public class DecimalNumberSystem implements INumberSystem {
+    static Pattern pattern = Pattern.compile("^\\d+([,.]\\d+)?$");
+    @Override
+    public Pattern getPattern() {
+        return pattern;
+    }
+
+    @Override
+    public double parse(String value) throws NumberFormatException {
+        return Double.parseDouble(value);
+    }
+
+    @Override
+    public String toString(double value) {
+        return String.valueOf(value);
+    }
 }
--- src/main/java/org/example/Calculator/numberSystems/INumberSystem.java
+++ src/main/java/org/example/Calculator/numberSystems/INumberSystem.java
@@ -1,2 +1,9 @@
-package org.example.Calculator.numberSystems;public interface INumberSystem {
+package org.example.Calculator.numberSystems;
+
+import java.util.regex.Pattern;
+
+public interface INumberSystem {
+    Pattern getPattern();
+    double parse(String value) throws NumberFormatException;
+    String toString(double value);
 }
